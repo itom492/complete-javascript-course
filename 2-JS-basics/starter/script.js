57b795ea8c244d109c09057cb2a7f18f0dfe784e
Hello <beth@Beths-MacBook-Pro.local>
2020-03-19 22:45:33 -0700

    first three lessons success

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+var firstName = 'Maxine';
+console.log(firstName);
+
+var lastname = 'Ito';
+var age = 21;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+var_3years = 3;
+var johnMark = 'John and Mark';
